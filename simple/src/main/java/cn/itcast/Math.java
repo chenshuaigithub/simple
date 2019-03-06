d350e018d8b41b9bd6456b3542221cd1ac801de4
zhangsan <zbangsan@qq.com>
2019-03-06 22:42:10 +0800

    添加到本地仓库

---
+++ simple/src/main/java/cn/itcast/Math.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class Math {
+    public static void main(String[] args) {
+        int x = 10;
+        double y = 3.3;
+        System.out.println(x / y);
+        System.out.println(x + y);
+    }
+}
